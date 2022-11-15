3fab1f1612bcc94958f4bc631a65283742172fac
jacgit18 <joshuaxcarpentier@gmail.com>
2022-11-15 15:44:26 -0500

    re enable port

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-
 const express = require("express");
 const app = express();
 
 // Heroku dynamically sets a port
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
 
